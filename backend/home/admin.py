81a73081463bea5fcb1bd7a19ba725a75f2632fa
Crowdbotics <team@crowdbotics.com>
2022-03-12 19:43:26 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Homepage
+
+admin.site.register(Homepage)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Homepage(models.Model):
+    "Generated Model"
+    name = models.IntegerField()
+    mobile = models.IntegerField()

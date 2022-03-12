ee0feeed6a960e344c5fb53125dd61d977755821
Crowdbotics <team@crowdbotics.com>
2022-03-12 19:45:14 +0000

    Update models

--- backend/home/migrations/0002_homepage.py
+++ backend/home/migrations/0002_homepage.py
@@ -8,16 +8,24 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Homepage',
+            name="Homepage",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.IntegerField()),
-                ('mobile', models.IntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.IntegerField()),
+                ("mobile", models.IntegerField()),
             ],
         ),
     ]
